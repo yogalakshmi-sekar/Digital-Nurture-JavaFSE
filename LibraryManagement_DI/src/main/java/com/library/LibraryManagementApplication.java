9ba8ef23d7d14fff66f0edac6c2d763580534c2d
yogalakshmi-sekar <145091449+yogalakshmi-sekar@users.noreply.github.com>
2026-07-05 21:34:00 +0530

    Add files via upload

---
+++ LibraryManagement_DI/src/main/java/com/library/LibraryManagementApplication.java
@@ -0,0 +1,21 @@
+package com.library;
+
+import com.library.service.BookService;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+public class LibraryManagementApplication {
+
+    public static void main(String[] args) {
+
+        ApplicationContext context =
+                new ClassPathXmlApplicationContext("applicationContext.xml");
+
+        BookService service =
+                context.getBean("bookService", BookService.class);
+
+        service.displayService();
+
+    }
+
+}
